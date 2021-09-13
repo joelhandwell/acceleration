0f94637533885a5e24593788827d4df7cec8bd87
Joel Handwell <joelhandwell@gmail.com>
2021-09-13 23:53:14 +0900

    Initial Commit

---
+++ src/main/kotlin/Main.kt
@@ -0,0 +1,21 @@
+import java.text.DecimalFormat
+import kotlin.math.absoluteValue
+import kotlin.math.pow
+
+// ref: https://www.desmos.com/calculator/2kkw81jkqu
+fun Double.format(): String = DecimalFormat("0.000").format(this)
+fun proportion(x: Double) = x
+fun square(x: Double) = x.pow(2).div(42 - 1)
+fun inverseProportion(x: Double) = (-1 - 42.0.div(x - 42.0)).absoluteValue
+
+fun main() {
+    listOf(::proportion, ::square, ::inverseProportion).forEach { function ->
+        println(function.name)
+
+        listOf(0.0, 0.5, 1.0, 10.0, 21.0, 41.0, 41.5, 41.9, 41.99, 41.999, 42.0).forEach { x ->
+            val y = function.call(x)
+            print("[${x.format()}, ${y.format().padStart(6)}], ")
+        }
+        println()
+    }
+}
